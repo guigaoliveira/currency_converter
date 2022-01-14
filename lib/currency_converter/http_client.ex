a7bbef70af71c6ecdc46f88880bf3b4a6176f7dc
Guilherme Oliveira <guilherme.borges@stone.com.br>
2022-01-13 22:23:11 -0300

    chore: simplify http client request result parse

--- lib/currency_converter/http_client.ex
+++ lib/currency_converter/http_client.ex
@@ -9,15 +9,8 @@ defmodule CurrencyConverter.HTTPClient do
   Possible error reasons.
   """
   @type error_reason ::
-          :bad_request
-          | :unauthenticated
-          | :unauthorized
-          | :not_found
-          | :unprocessable_entity
-          | :client_error
-          | :server_error
+          :unavailable
           | :unexpected_return
-          | :unauthorized
 
   @typedoc """
   Possible bodies in an HTTP response.
@@ -45,9 +38,6 @@ defmodule CurrencyConverter.HTTPClient do
           {:ok, success_response()}
           | {:error, {error_reason(), body()} | :unexpected_return | error_reason()}
 
-  defguardp is_redirect(status) when is_integer(status) and status >= 300 and status < 400
-  defguardp is_client_error(status) when is_integer(status) and status >= 400 and status < 500
-  defguardp is_server_error(status) when is_integer(status) and status >= 500
   defguardp is_success(status) when is_integer(status) and status >= 200 and status < 300
 
   @spec new(Keyword.t()) :: Tesla.Client.t()
@@ -83,44 +73,12 @@ defmodule CurrencyConverter.HTTPClient do
     {:ok, %{body: env.body, headers: env.headers, status: status}}
   end
 
-  defp parse_result({:ok, %{status: 400, body: body}}) do
-    Logger.warning("Bad request. Body: #{inspect(body)}")
-    {:error, {:bad_request, body}}
+  defp parse_result({:ok, %{status: status, body: body}}) do
+    Logger.warning("#{__MODULE__}: New error when #{status}, body: #{inspect(body)}")
+    {:error, :unavailable}
   end
-
-  defp parse_result({:ok, %{status: 422, body: body}}) do
-    Logger.warning("Unprocessable entity. Body: #{inspect(body)}")
-    {:error, {:unprocessable_entity, body}}
-  end
-
-  defp parse_result({:ok, %{status: 409, body: body}}) do
-    Logger.warning("Conflict. Body: #{inspect(body)}")
-    {:error, {:conflict, body}}
-  end
-
-  defp parse_result({:ok, %{status: 401, body: body}}), do: {:error, {:unauthenticated, body}}
-
-  defp parse_result({:ok, %{status: 403, body: body}}),
-    do: {:error, {:unauthorized, body}}
-
-  defp parse_result({:ok, %{status: 404}}), do: {:error, :not_found}
-
-  defp parse_result({:ok, %{status: 503}}), do: {:error, :service_unavailable}
-
-  defp parse_result({:ok, %{status: status, body: body}}) when is_client_error(status),
-    do: {:error, {:client_error, body}}
-
-  defp parse_result({:ok, %{status: status, body: body}}) when is_redirect(status),
-    do: {:error, {:redirect_error, body}}
-
-  defp parse_result({:ok, %{status: status, body: body}}) when is_client_error(status),
-    do: {:error, {:client_error, body}}
-
-  defp parse_result({:ok, %{status: status, body: body}}) when is_server_error(status),
-    do: {:error, {:server_error, body}}
 
   defp parse_result({:error, _reason} = err), do: err
-  defp parse_result(_), do: {:error, :unexpected_return}
 
   defp config(key) do
     Application.fetch_env!(:currency_converter, __MODULE__) |> Keyword.fetch!(key)
